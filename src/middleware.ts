d3dc60a4e5683fda3dc369599af0d7653776ac8b
Jacques Masuruku <jacquesmasuruku2@gmail.com>
2026-07-19 18:35:27 +0200

    Completely disable middleware to fix redirect loop

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,2 @@
-// Temporarily disabled to fix redirect loop
-// import createMiddleware from 'next-intl/middleware';
-
-export default function middleware(request: Request) {
-  // Disabled middleware to prevent redirect loop
-  return;
-}
-
-// export default createMiddleware({
-//   // A list of all locales that are supported
-//   locales: ['fr', 'en'],
-
-//   // Used when no locale matches
-//   defaultLocale: 'fr'
-// });
-
-export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
-};
+// Disabled middleware to fix redirect loop
+// The site will use single-language routing for now
