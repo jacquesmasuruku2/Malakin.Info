ddd717690bbce1fffee30b72ea4bbb4d48a8ad43
Jacques Masuruku <jacquesmasuruku2@gmail.com>
2026-07-20 16:18:58 +0200

    Remove next-intl plugin from config to fix build error

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
-
-const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['127.0.0.1'],
 };
 
-export default withNextIntl(nextConfig);
+export default nextConfig;
